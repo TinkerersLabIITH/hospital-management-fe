042c93a2cc64413f5c899556f85487f401f78732
Pranjal095 <pranjal.prajapati35@gmail.com>
2024-07-16 19:35:17 +0530

    Login page added with Google authentication

--- README.md
+++ README.md
@@ -6,3 +6,24 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Setting Up the Development Environment
+1. **Clone the Repository**
+   ```bash
+   git clone https://github.com/TinkerersLabIITH/hospital-management-fe.git
+   cd hospital-management-fe
+
+2. **Install Dependencies**
+    ```bash
+    npm install
+
+3. **Configure Environment Variables**
+    - Create a .env file in the root directory and add the necessary environment variables:
+    ```bash
+    VITE_FIREBASE_API_KEY="AIzaSyDwxG1JYySORI7wQn1zWeZInw0U2uujcvo"
+	VITE_FIREBASE_AUTH_DOMAIN="example"
+	VITE_FIREBASE_PROJECT_ID="example"
+	VITE_FIREBASE_STORAGE_BUCKET="example"
+	VITE_FIREBASE_MESSAGING_SENDER_ID="example"
+	VITE_FIREBASE_APP_ID="example"
+	VITE_SERVER_URI="http://localhost:5000" (for local hosting)
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import './App.css'
+import React from 'react';
+import { signInWithGoogle } from './firebaseConfig';
+import axios from 'axios';
 
 function App() {
+  const handleLogin = async () => {
+    try{
+      const result = await signInWithGoogle();
+      const { user } = result;
+      const emailID = user["email"];
+      const name = user["displayName"];
+
+      const response = await axios.post(import.meta.env.VITE_SERVER_URI+'/api/auth/fetch',{ emailID: emailID });
+
+      if(response.data["authLevel"]){
+        alert(`Welcome, ${name} (${response.data["authLevel"]})`);
+      }
+    }
+    catch(error){
+      console.error(error);
+      alert('Authentication failed');
+    }
+  };
 
   return (
     <>
       <div className='w-screen text-center'>
-        Code Here
+        <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+          <h1 className="text-center">Hospital Management System</h1>
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={handleLogin}>
+            Sign in with Google
+          </button>
+        </div>
       </div>
     </>
   )
