28ae7927c7da67e5119a51e1ce7b0172f5992809
jimmy2683 <karangupta0805@gmail.com>
2024-08-07 22:17:34 +0530

    AddEnrty.jsx added and now app is responsive and proscription page added

--- src/components/Priscription.jsx
+++ src/components/Priscription.jsx
@@ -7,8 +7,7 @@ function Priscription() {
         <div className="">
             <Header Department={"Pharmacy"} />
             <div className="overflow-x-auto overflow-y-scroll">
-                <table className="min-w-full divide-y divide-gray-200">
-                    Recent
+                <table className="min-w-full divide-y divide-gray-200 ml-10">
                     <thead className="bg-gray-50">
                         <tr>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Medicine Name</th>
